611fa0f7e73351bb3299cd569f878e6bb6f59f8c
giuseppesciacca <sciaccagi@gmail.com>
2023-05-26 18:13:26 +0200

    [add] seeder

--- database/seeders/ArtistSeeder.php
+++ database/seeders/ArtistSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Artist;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Faker\Generator as Faker;
@@ -15,6 +16,16 @@ class ArtistSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
-        //
+        for ($i = 0; $i < 10; $i++) {
+            $Artist = new Artist();
+            $Artist->name = $faker->firstName();
+            $Artist->lastname = $faker->lastName();
+            $Artist->stage_name = $faker->company();
+            $Artist->date_of_birth = $faker->dateTime();
+            $Artist->record_label = $faker->company();
+            $Artist->genre = $faker->randomElement(['rock', 'jazz', 'reggaeton', 'latino', 'prog']);;
+            $Artist->album = $faker->word(50);
+            $Artist->save();
+        }
     }
 }
