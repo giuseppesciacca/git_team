d5f304f812196ab511b9616810df6a4de4eb2f62
nicolaazonta <nicolazonta114@gmail.com>
2023-06-06 16:11:00 +0200

    make show CRUD

---
+++ app/Http/Controllers/Admin/ArtistController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Models\Artist;
+use App\Http\Requests\StoreArtistRequest;
+use App\Http\Requests\UpdateArtistRequest;
+use App\Http\Controllers\Controller;
+
+class ArtistController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $artists = Artist::all();
+        return view('admin.artists.index', compact('artists'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreArtistRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreArtistRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Artist  $artist
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Artist $artist)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Artist  $artist
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Artist $artist)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateArtistRequest  $request
+     * @param  \App\Models\Artist  $artist
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateArtistRequest $request, Artist $artist)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Artist  $artist
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Artist $artist)
+    {
+        //
+    }
+}
